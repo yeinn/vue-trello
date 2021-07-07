cdb9666a2e88bf5e9f7f5314bf3eeaa6529d91e7
최 예인 <yeindev@gmail.com>
2021-07-07 17:14:46 +0900

    board list 조회, 생성 구현

--- src/component/Login.vue
+++ src/component/Login.vue
@@ -1,8 +1,83 @@
 <template>
-  <div>
-    Login
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="email">Email</label>
+        <input
+          class="form-control"
+          type="text"
+          name="email"
+          v-model="email"
+          autofocus
+          placeholder="e.g., test@test.com"
+        />
+      </div>
+      <div>
+        <label for="password">Passwrod</label>
+        <input
+          class="form-control"
+          type="password"
+          v-model="password"
+          placeholder="123123"
+        />
+      </div>
+      <button
+        class="btn"
+        :class="{ 'btn-success': !invalidForm }"
+        type="submit"
+        :disabled="invalidForm"
+      >
+        Log In
+      </button>
+    </form>
+    <p class="error" v-if="error">{{ error }}</p>
   </div>
 </template>
+
 <script>
-export default {};
+import { auth, setAuthInHeader } from "../api";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: "",
+      returnURL: ""
+    };
+  },
+  computed: {
+    invalidForm() {
+      return !this.email || !this.password;
+    }
+  },
+  created() {
+    this.returnURL = this.$route.query.returnURL || "/";
+  },
+  methods: {
+    onSubmit() {
+      auth
+        .login(this.email, this.password)
+        .then(data => {
+          localStorage.setItem("token", data.accessToken);
+          setAuthInHeader(data.accessToken);
+          this.$router.push(this.returnURL);
+        })
+        .catch(err => {
+          this.error = err.data.error;
+        });
+    }
+  }
+};
 </script>
+
+<style>
+.login {
+  width: 400px;
+  margin: 0 auto;
+}
+.error {
+  color: #f00;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,21 @@ import NotFound from "../component/NotFound";
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem("token");
+  const loginPath = `/login?returnURL=${encodeURIComponent(to.path)}`;
+  isAuth ? next() : next(loginPath);
+};
+
 const router = new VueRouter({
   mode: "history",
   routes: [
-    { path: "/", component: Home },
+    { path: "/", component: Home, beforeEnter: requireAuth },
     { path: "/login", component: Login },
     {
       path: "/b/:bid",
       component: Board,
+      beforeEnter: requireAuth,
       children: [{ path: "c/:cid", component: Card }]
     },
     { path: "*", component: NotFound }
